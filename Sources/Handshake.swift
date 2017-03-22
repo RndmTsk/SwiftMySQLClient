701e2c567583eb4df8c8ae7cea844c571b2378f7
Terry Latanville <terry.latanville@tulip.io>
2017-03-22 17:17:51 -0400

    Fixed up issues with handshake based on golang driver reference

--- Sources/Handshake.swift
+++ Sources/Handshake.swift
@@ -15,11 +15,10 @@ internal extension MySQL {
         let protocolVersion: Int
         let serverVersion: String
         let connectionID: Int
-        let authPluginDataPart1: String
+        let authCipher: String
         let capabilityFlags: CapabilityFlag // NOTE: In position 6 & 9
         let characterset: Int
         let statusFlags: StatusFlag
-        let authPluginLength: Int
         let authPluginName: String
 
         var description: String {
@@ -35,42 +34,39 @@ internal extension MySQL {
             self.connectionID = result.removingInt(of: 4)
 
             // 2 bytes for auth plugin data -- 8 bytes
-            self.authPluginDataPart1 = result.removingString(of: 8)
+            var authCipher = result.removingString(of: 8)
 
             // 1 byte of filler (unused at the moment)
             result.droppingFirst(1)
 
             // Optional pieces after the filler //
-            var capFlag1: CapabilityFlag = []
+
             // 2 bytes for capability flag part 1
-            if result.count > 1 {
-                capFlag1 = CapabilityFlag(rawValue: UInt32(result.removingInt(of: 2)))
-            }
+            let capLower = CapabilityFlag(rawValue: UInt32(result.removingInt(of: 2)))
 
             // 1 byte for character sets
-            if result.count > 0 {
-                self.characterset = result.removingInt(of: 1)
-            } else {
-                self.characterset = 0 // TODO: (TL) throw? abort?
-            }
+            self.characterset = result.removingInt(of: 1)
 
             // 2 bytes for status flags
-            if result.count > 1 {
-                self.statusFlags = StatusFlag(rawValue: UInt16(result.removingInt(of: 2)))
-            } else {
-                self.statusFlags = []
-            }
+            self.statusFlags = StatusFlag(rawValue: UInt16(result.removingInt(of: 2)))
 
             // 2 bytes for upper capability flags
-            var capFlag2: CapabilityFlag = []
-            if result.count > 1 {
-                capFlag2 = CapabilityFlag(rawValue: UInt32(result.removingInt(of: 2) << 16))
-            }
-            self.capabilityFlags = CapabilityFlag.with(upper: capFlag2, lower: capFlag1)
+            let capUpper = CapabilityFlag(rawValue: UInt32(result.removingInt(of: 2) << 16))
+            self.capabilityFlags = CapabilityFlag.with(upper: capUpper, lower: capLower)
 
             // TODO: (TL) What do we do with this?
             // 1 byte for plugin length
-            self.authPluginLength = result.removingInt(of: 1)
+            let authPluginLength = result.removingInt(of: 1)
+
+            // 10 bytes are reserved
+            result.droppingFirst(10)
+
+            // According to the Python and golang drivers
+            // the other half is 12 bytes
+            // This seems to agree with authPluginLength (21) - first bytes (8) - \0 (1)
+            authCipher.append(result.removingString(of: authPluginLength - 9))
+
+            self.authCipher = authCipher
             self.authPluginName = result.removingNullEncodedString()
         }
     }
